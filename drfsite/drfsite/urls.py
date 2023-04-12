88cf4db63503175589d35ace88c9a9ebad575e15
Mikhail <dima.pupok2011@yandex.ru>
2023-04-12 23:35:29 +0300

    django drf begining

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from mendrf.views import *
 from rest_framework import routers
 
@@ -39,7 +39,10 @@ from rest_framework import routers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/drf-auth/', include('rest_framework.urls')),
     path('api/v1/men/', MenAPIList.as_view()),
     path('api/v1/men/<int:pk>/',MenAPIUpdate.as_view()),
     path('api/v1/mendelete/<int:pk>/',MenApiDestroy.as_view()),
+    path(r'api/v1/auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
--- drfsite/mendrf/views.py
+++ drfsite/mendrf/views.py
@@ -2,7 +2,7 @@ from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics, viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -35,7 +35,7 @@ class MenAPIList(generics.ListCreateAPIView):
 class MenAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = Men.objects.all()
     serializer_class = MenSerializer
-    permission_classes = (IsOwnerOrReadOnly, )
+    permission_classes = (IsAuthenticated, )
 
 class MenApiDestroy(generics.RetrieveDestroyAPIView):
     queryset = Men.objects.all()
