bc2c70e48767a41d2e58c73929c1f1f6420d9970
Mikhail <dima.pupok2011@yandex.ru>
2023-04-25 00:01:59 +0300

    to do app

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from mendrf.views import *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView, TokenVerifyView,
+)
 from rest_framework import routers
 
 #class MyCustomRouter(routers.SimpleRouter):
@@ -45,4 +49,7 @@ urlpatterns = [
     path('api/v1/mendelete/<int:pk>/',MenApiDestroy.as_view()),
     path(r'api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
